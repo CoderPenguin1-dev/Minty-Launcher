c8ad26cb4b836393203c3ebf83db88e71d9eebc3
CoderPenguin1 <chapman.calebh@icloud.com>
2025-02-02 23:11:46 -0500

    Remove unneeded PRERELEASE notice checker.

--- Doom Loader/About.cs
+++ Doom Loader/About.cs
@@ -21,17 +21,10 @@ namespace Doom_Loader
 
         private void About_Load(object sender, EventArgs e)
         {
-            if (GetType().Assembly.GetName().Version.MinorRevision == 1) // If the version of Minty Launcher is a pre-release version
-                version.Text = "PRE-RELEASE Version " +
-                    $"{GetType().Assembly.GetName().Version.Major}." +
-                    $"{GetType().Assembly.GetName().Version.Minor}." +
-                    $"{GetType().Assembly.GetName().Version.Build}";
-
-            else
-                version.Text = "Version " +
-                    $"{GetType().Assembly.GetName().Version.Major}." +
-                    $"{GetType().Assembly.GetName().Version.Minor}." +
-                    $"{GetType().Assembly.GetName().Version.Build}";
+            version.Text = "Version " +
+                $"{GetType().Assembly.GetName().Version.Major}." +
+                $"{GetType().Assembly.GetName().Version.Minor}." +
+                $"{GetType().Assembly.GetName().Version.Build}";
         }
 
         // Little Easter Egg
