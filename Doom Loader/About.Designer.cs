a2664ce3cda07c7d355be6060b35621e764cadbe
CoderPenguin1 <chapman.calebh@icloud.com>
2025-04-03 13:00:56 -0400

    Fix wrongly scaled logo

--- Doom Loader/About.Designer.cs
+++ Doom Loader/About.Designer.cs
@@ -43,7 +43,7 @@
             pictureBox1.Location = new Point(41, 3);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(228, 203);
-            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
             // 
